003ba06f34fd1b40446e7015ca4781c948a3851e
dsound-zz <demiansims@gmail.com>
2020-09-07 17:45:12 -0400

    mine

--- cards.rb
+++ cards.rb
@@ -2,113 +2,194 @@ SUITS = [:spades, :diamonds, :hearts, :clubs]
 VALUES = (1..13).to_a
 FACES = ["Jack", "Queen", "King", "Ace"]
 
+
 class Card
-  attr_accessor :suit, :value
-  
-  def initialize(suit, value)
-    @suit = suit
-    @value = value
-  end
-  
-  def to_s
-    suit = @suit.to_s.capitalize
-    if @value >= 0 && @value < 10
-      "#{@value + 1} of #{suit}"
-    else
-      "#{FACES[@value - 10]} of #{suit}"
+    attr_accessor :suit, :value
+    
+    def initialize(*suit, value) 
+        self.suit = suit
+        self.value = value
     end
-  end
-end
-
-class Hand
-  attr_accessor :cards
-
-  def initialize(cards)
-    @cards = cards
-  end
-  
-  def beats?(other)
-   score > other.score
-  end
-
-  def score
-    unit = 1000
-    total = 0
-    total += unit * 10 if is_straight && is_flush
-    total += unit * 8 if of_a_kind == 4
-    total += unit * 7 if is_full_house
-    total += unit * 6 if is_flush
-    total += unit * 5 if is_straight
-    total += unit * 4 if !is_full_house && of_a_kind == 3
-    total += unit * 3 if is_two_pair
-    total += unit * 2 if !is_two_pair && of_a_kind == 2
-    if total >= 2000
-      total += total_value
-    end
-    total += high_card.value
-    total
-  end
-
-  def is_full_house
-    values = @cards.group_by(&:value).values
-    values.length == 2 && values.all? {|v| [2,3].include? v}
-  end
-
-  def is_flush
-    by_value = @cards.sort_by(&:value).map(&:suit).uniq.length == 1
-  end
-
-  def is_straight
-    last_card = nil
-    differences = []
-    @cards.sort_by(&:value).each do |card| 
-      if last_card
-        differences.push(card.value - last_card.value)
-      end
-      last_card = card
-    end
-    differences.uniq.length == 1 && differences.first == 1
-  end
-
-  def is_two_pair
-    values = @cards.group_by(&:value).values.sort_by(&:length)
-    values.length == 3 && values.last.length == 2
-  end
-  
-  def of_a_kind
-    pairs_up = @cards.group_by(&:value).values.sort_by(&:length).filter{|g| g.length > 1}
-    pairs_up.last && pairs_up.last.length
-  end
-  
-  def total_value
-    @cards.map(&:value).reduce(:+)
-  end
-  
-  def high_card
-    @cards.sort_by(&:value).last
-  end
-
 end
 
 class Deck
-  attr_reader :cards
-  
-  def initialize
-    @cards = SUITS.flat_map do |suit|
-      VALUES.map do |value| 
-        Card.new(suit, value)
-      end
+    attr_reader :cards
+
+    def initialize
+        cards = VALUES.flat_map do |value| 
+            SUITS.map do |suit| 
+                Card.new(suit, value)
+            end
+        end
+        @cards = cards
     end
-  end
-  
-  def shuffle
-    @cards.shuffle!
-  end
-  
-  def draw(amount)
-    cards.pop(amount)
-  end
+
+    def shuffle
+        self.cards.map(&:value).shuffle
+    end
+
 end
 
+class Hand 
+    attr_reader :hand    
+
+    def initialize 
+      @hand = Deck.new.cards.sample(5)
+    end
+end
+
+class Game 
+
+    def initialize 
+        @hand1 = Hand.new.hand.map { |h| h.suit.push(h.value) } 
+        @hand2 = Hand.new.hand.map { |h| h.suit.push(h.value) }  
+    end 
+
+    def play 
+        hand1 = score(@hand1)
+        hand2 = score(@hand2)
+        # High card 
+        if hand1[0] == hand2[0] 
+            if hand1[1] > hand2[1] 
+                puts "#{@hand1} beats #{@hand2}"
+            else 
+                puts "#{@hand2} beats #{@hand1}"
+            end 
+            return
+        end
+        # Other hands
+        if hand1[0] > hand2[0] 
+            puts "#{@hand1} beats #{@hand2}"
+        else 
+            puts "#{@hand2} beats #{@hand1}"
+        end
+        
+    end
 
 
+    private
+
+    def score(hand)
+        suit_count = {
+            :spades => 0,
+            :clubs => 0,
+            :hearts => 0,
+            :diamonds => 0
+        }
+
+        value_count = {
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+            6 => 0, 
+            7 => 0, 
+            8 => 0, 
+            9 => 0,
+            10 => 0, 
+            11 => 0,
+            12 => 0,
+            13 => 0
+        }
+
+        # hand = Hand.new 
+
+        # hand = hand.hand.map { |h| h.suit.push(h.value) }
+    
+
+        hand.each do |h| 
+            suit_count[h[0]] += 1
+            value_count[h[1]] += 1
+        end
+
+        final_hand_score = 0
+        pair = 0
+        three_of_kind = 0
+        high_card = 0
+        straight_check = []
+
+
+        value_count.each do |k,v| 
+            if v > 0 
+            high_card = k
+            current = high_card 
+            if k > current  
+                    high_card = k
+            else   
+                    high_card = current 
+            end 
+            straight_check.push(k)
+            end
+            
+            if v == 2 
+                pair += 1 
+            end
+
+            if v == 3 
+                three_of_kind = 1
+            end
+
+            if pair == 1 # pair
+                final_hand_score = 2
+            elsif pair == 2 # two pair
+                final_hand_score = 3 
+            elsif three_of_kind == 1 # three of a kind
+                final_hand_score = 4
+            elsif straight(straight_check) # straight
+                if straight(straight_check) && royal_flush_check(straight_check)  
+                    final_hand_score = 9 # royal flush 
+                elsif flush(suit_count) 
+                    final_hand_score = 8 # straight flush
+                else    
+                    final_hand_score = 5  # straight  
+                end
+            elsif flush(suit_count) # flush
+                    final_hand_score = 6 # flush
+            else     
+                final_hand_score = 1     
+            end
+        end
+        if pair == 1 && three_of_kind == 1
+            final_hand_score = 7 # full house
+        end
+        [final_hand_score, high_card]
+    end
+
+    # method for straight
+    def straight(ary)  
+        count = 0 
+        card = ary[0]
+        ary.each do |s| 
+            if s - card == 1
+                card = s 
+                count += 1
+            end    
+        end
+        if count == 4 
+            return true
+        else 
+            return false
+        end
+    end
+
+    # method for flush
+    def flush(suits) 
+        suits.each do |k, v| 
+            if suits[k] == 5 
+                return true 
+            else   
+                return false  
+            end
+        end 
+    end
+
+    # method for roayl_flush
+    def royal_flush_check(ary)
+        if ary == (9..13).to_a 
+            return true 
+        else 
+            return false 
+        end 
+    end
+end
